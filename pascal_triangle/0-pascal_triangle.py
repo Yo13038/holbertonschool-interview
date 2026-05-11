94728327869ff310b40bf419c3aaf8bbf9f1665e
Yo13038 <13038@holbertonstudents.com>
2026-05-11 13:28:12 +0200

    pascale triangle with annotations include

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -10,13 +10,13 @@ def pascal_triangle(n):
     #create rows
     for i in range(1, n):
         prev_row = triangle[-1]
-        new_row = [1]# top of triangle
+        new_row = [1]# always star with a 1
 
         #num between the edge
         for j in range(1, i):
             new_row.append(prev_row[j-1] + prev_row[j])
 
-        new_row.append(1)
-        triangle.append(new_row)
+        new_row.append(1)# always end with 1
+        triangle.append(new_row)#add the new line
     
     return triangle
