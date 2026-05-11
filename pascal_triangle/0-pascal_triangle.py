16e8eed2520b01a318f57b803d89214ab613a75e
Yo13038 <13038@holbertonstudents.com>
2026-05-11 13:33:35 +0200

    add the pycodestyle requirement

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -3,20 +3,20 @@
 def pascal_triangle(n):
     if n <= 0:
         return []
-    
-    #initialisation triangle
+
+    # initialisation triangle
     triangle = [[1]]
 
-    #create rows
+    # create rows
     for i in range(1, n):
         prev_row = triangle[-1]
-        new_row = [1]# always star with a 1
+        new_row = [1]  # always star with a 1
 
-        #num between the edge
+        # num between the edge
         for j in range(1, i):
             new_row.append(prev_row[j-1] + prev_row[j])
 
-        new_row.append(1)# always end with 1
-        triangle.append(new_row)#add the new line
-    
+        new_row.append(1)  # always end with 1
+        triangle.append(new_row)  # add the new line
+
     return triangle
