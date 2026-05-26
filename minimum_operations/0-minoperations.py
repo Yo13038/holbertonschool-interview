459675f903dd5019e32b9f598bf3b29d823dd72e
Yo13038 <13038@holbertonstudents.com>
2026-05-26 10:28:19 +0200

    methode to calculate the minimum operation to reach a number

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Module calculates the fewest number of operations needed
+to result in exactly n H characters in the file.
+"""
+
+
+def minOperations(n):
+    """function to handle the minimum operation"""
+    if n <= 1:
+        return 0
+
+    operation = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operation += factor
+            n = n // factor
+        factor += 1
+    return operation
