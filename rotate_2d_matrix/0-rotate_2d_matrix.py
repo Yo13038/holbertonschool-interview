a27e66683d3ea786ea95c1010f5a5b300d7e686a
Yo13038 <13038@holbertonstudents.com>
2026-06-29 11:24:12 +0200

    add pycodestyle requirements

--- rotate_2d_matrix/0-rotate_2d_matrix.py
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """rotate 2D matrix
 """
+
+
 def rotate_2d_matrix(matrix):
     """matrix rotating"""
 
     n = len(matrix)
 
     for i in range(n):
-        for j in range(i,n):
-            temp = matrix [i][j]
-            matrix [i][j] = matrix [j][i]
-            matrix [j][i] = temp
-    
+        for j in range(i, n):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp
+
     for i in range(n):
         matrix[i].reverse()
-
